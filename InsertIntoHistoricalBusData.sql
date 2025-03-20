4ecc5bc9f941999ab1d39e4f1fa224edbb64a3fe
Sanji <147115096+sanjixthapa@users.noreply.github.com>
2025-03-20 15:44:18 -0400

    Delete InsertIntoHistoricalBusData.sql

--- InsertIntoHistoricalBusData.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO HistoricalBusData (BusID, RouteID, Latitude, Longitude, Speed, Timestamp)
-SELECT BusID, RouteID, Latitude, Longitude, Speed, Timestamp
-FROM RealTimeBusData
-WHERE Timestamp < NOW() - INTERVAL 1 DAY;
-
-DELETE FROM RealTimeBusData WHERE Timestamp < NOW() - INTERVAL 1 DAY;
