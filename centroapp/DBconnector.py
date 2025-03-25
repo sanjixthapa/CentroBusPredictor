0235026ca70e053775743e98f309c47302eb251d
Sanji <147115096+sanjixthapa@users.noreply.github.com>
2025-03-25 06:58:45 -0400

    Update DBconnector.py

--- centroapp/DBconnector.py
+++ centroapp/DBconnector.py
@@ -3,8 +3,8 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 #db config
 host="pi.cs.oswego.edu",
-username="",#username
-password="",#pass
+username="CSC380_25S_TeamA",#username
+password="csc380_25s",#pass
 database_name="CSC380_25S_TeamA"
 
 db_url = f"mysql+pymysql://{username}:{password}@{host}/{database_name}"
