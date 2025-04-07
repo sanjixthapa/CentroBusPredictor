c06e8c8b15fa2dfe85e79d64e4f17df2ce8fc5c2
Sanji <147115096+sanjixthapa@users.noreply.github.com>
2025-04-07 09:40:14 -0400

    Delete mySQL/SelectFrom.sql

--- mySQL/SelectFrom.sql
+++
@@ -1,5 +0,0 @@
-SELECT h.RouteID, h.Latitude, h.Longitude, h.Timestamp, w.Temperature, w.Precipitation
-FROM HistoricalBusData h
-JOIN WeatherData w
-ON DATE(h.Timestamp) = DATE(w.Timestamp)
-ORDER BY h.Timestamp DESC;
