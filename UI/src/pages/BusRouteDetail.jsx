b24f91197c11008992bb45b8e8de0728f2a4681d
Oak Soe Zaw <ozaw@oswego.edu>
2025-04-21 10:44:49 -0400

    Add files via upload

---
+++ UI/src/pages/BusRouteDetail.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useParams } from 'react-router-dom';
+import { getStopBack, getStopTo } from '../services/getBusRoute';
+import StopCard from '../components/stopCard';
+import '../css/Home.css'
+
+function BusRouteDetail() {
+
+  // Step 2: Destructure and log the specific param
+  const { routeID } = useParams();
+  console.log("busRoute:", routeID);
+
+  const {stopsBack, loading: loadingBack, error: errorBack} = getStopBack(routeID);
+  const {stopsTo, loading: loadingTo , error: errorTo } = getStopTo(routeID);
+
+  console.log(stopsBack)
+
+  if (loadingTo || loadingBack) return <div>Loading...</div>;
+  if (errorTo || errorBack) return <div>Error loading data.</div>;
+
+  return (
+    <div className='stops-information'>
+        <div className='bus-to'>
+          {stopsTo.map(stop => <StopCard  stop={stop} key={stop.stop_id} />)}
+        </div>
+        <div className='bus-from'>
+          {stopsBack.map(stop => <StopCard  stop={stop} key={stop.stop_id} />)}
+        </div>
+    </div>
+  )
+}
+
+export default BusRouteDetail;
